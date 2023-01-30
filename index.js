568e24b2151363017f98a9e0376eed974b22e87f
Anchovy <114173241+anchovydev@users.noreply.github.com>
2023-01-30 18:57:23 -0500

    fix #1 in caveman way

--- index.js
+++ index.js
@@ -14,15 +14,17 @@ client.on("messageCreate", message => {
    if (message.author.id == userId) return;
 
    // goofy ahh case sensitivity
-   if (message.content.toLowerCase().includes("im") || message.content.toLowerCase().includes("i am") || message.content.toLowerCase().includes("i\'m")) {
+   if (message.content.toLowerCase().indexOf("im") !== -1 || message.content.toLowerCase().indexOf("i am") !== -1 || message.content.toLowerCase().indexOf("i\'m") !== -1) {
       console.log(`message sent by ${message.author.username} in server \"${message.guild.name}\"\ncontent: \"${message.content}\"`);
-         const newIndex = Math.max(message.content.toLowerCase().indexOf("i am") + 4, message.content.toLowerCase().indexOf("im") + 2, message.content.toLowerCase().indexOf("i\'m") + 3);
-         const newMessage = message.content.slice(newIndex).trim();
-         const channel = message.channel;
+      var newIndex = Math.max(message.content.toLowerCase().indexOf("i am") + 4, message.content.toLowerCase().indexOf("im") + 2, message.content.toLowerCase().indexOf("i\'m") + 3);
+      if (newIndex == 3 && message.content.toLowerCase().startsWith("im")) {
+         newIndex = 2;
+      }
+      const newMessage = message.content.slice(newIndex).trim();
+      const channel = message.channel;
          if (!newMessage == "") {
             channel.send(`hi ${newMessage}, i'm dad-bot`);
          }
-         console.log(newIndex);
    }
 });
 
