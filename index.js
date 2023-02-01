d8387ff93a9e8f40d7ca92bfda6c5781e33fe335
Anchovy <114173241+anchovydev@users.noreply.github.com>
2023-01-31 21:10:22 -0500

    remove console.log because bot is fully operational

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const { token, userId } = require("./config.json");
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once("ready", () => {
-	console.log("signed in");
    client.user.setActivity("getting the milk");
 });
 
@@ -16,15 +15,13 @@ client.on("messageCreate", message => {
 
    // goofy ahh case sensitivity
    if (message.content.toLowerCase().indexOf("im") !== -1 || message.content.toLowerCase().indexOf("i am") !== -1 || message.content.toLowerCase().indexOf("i\'m") !== -1) {
-      console.log(`message sent by ${message.author.username} in server \"${message.guild.name}\"\ncontent: \"${message.content}\"`);
       var newIndex = Math.max(message.content.toLowerCase().indexOf("i am") + 4, message.content.toLowerCase().indexOf("im") + 2, message.content.toLowerCase().indexOf("i\'m") + 3);
       if (newIndex == 3 && message.content.toLowerCase().startsWith("im")) {
          newIndex = 2;
       }
       const newMessage = message.content.slice(newIndex).trim();
-      const channel = message.channel;
          if (!newMessage == "") {
-            channel.send(`hi ${newMessage}, i'm dad-bot`);
+            message.channel.send(`hi ${newMessage}, i'm dad-bot`);
          }
    }
 });
