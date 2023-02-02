64519bb262f46061661f238cabff2ed325b32330
Anchovy <114173241+anchovydev@users.noreply.github.com>
2023-02-01 19:39:46 -0500

    this will probably be the last commit. all i did was fix the indents. while i'm really proud of this bot, i think it's time i moved on to something else. expect dad-bot to be online sometimes - not 24/7.

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ client.on("messageCreate", message => {
       if (newIndex == 3 && message.content.toLowerCase().startsWith("im")) {
          newIndex = 2;
       }
-         if (!message.content.slice(newIndex).trim() == "") {
-            message.channel.send(`hi ${message.content.slice(newIndex).trim()}, i'm dad-bot`);
-         }
+      if (!message.content.slice(newIndex).trim() == "") {
+         message.channel.send(`hi ${message.content.slice(newIndex).trim()}, i'm dad-bot`);
+      }
    }
 });
 
