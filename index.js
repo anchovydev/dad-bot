0e19c71c05f50cb79327911e20e97e51ceeef66b
anchovy <114173241+anchovydev@users.noreply.github.com>
2024-04-18 21:08:03 -0400

    clown moment
    ainnoway i forgor "i m" trigger 💀

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ client.on("messageCreate", message => {
    if (message.content.indexOf("@") !== -1) return;
 
    // goofy ahh case sensitivity
-   if (message.content.toLowerCase().indexOf("im") !== -1 || message.content.toLowerCase().indexOf("i am") !== -1 || message.content.toLowerCase().indexOf("i\'m") !== -1 || message.content.toLowerCase().indexOf("iam") !== -1) {
-      var newIndex = Math.max(message.content.toLowerCase().indexOf("i am") + 4, message.content.toLowerCase().indexOf("im") + 2, message.content.toLowerCase().indexOf("i\'m") + 3, message.content.toLowerCase().indexOf("iam") + 3);
+   if (message.content.toLowerCase().indexOf("im") !== -1 || message.content.toLowerCase().indexOf("i am") !== -1 || message.content.toLowerCase().indexOf("i\'m") !== -1 || message.content.toLowerCase().indexOf("iam") !== -1 || message.content.toLowerCase().indexOf("i m") !== -1) {
+      var newIndex = Math.max(message.content.toLowerCase().indexOf("i am") + 4, message.content.toLowerCase().indexOf("im") + 2, message.content.toLowerCase().indexOf("i\'m") + 3, message.content.toLowerCase().indexOf("iam") + 3, message.content.toLowerCase().indexOf("i m") + 3);
       if (newIndex == 3 && message.content.toLowerCase().startsWith("im")) {
          newIndex = 2;
       }
